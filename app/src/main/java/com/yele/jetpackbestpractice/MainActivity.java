6c9136c5938bd9ef0baaa961f545b5434226e017
flyli <897321637@qq.com>
2021-08-03 23:59:27 +0800

    第二次

--- app/src/main/java/com/yele/jetpackbestpractice/MainActivity.java
+++ app/src/main/java/com/yele/jetpackbestpractice/MainActivity.java
@@ -1,8 +1,10 @@
 package com.yele.jetpackbestpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class ListenerHandler extends DrawerLayout.SimpleDrawerListener {
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            mState.isDrawerOpened.set(true);
+        }
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            super.onDrawerClosed(drawerView);
+            mState.isDrawerOpened.set(false);
+            mState.openDrawer.setValue(false);
+        }
     }
 }
